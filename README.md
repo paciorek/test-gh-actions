78aeb86bdbad56f89e03e895c2dd667073b453f0
Christopher Paciorek <paciorek@stat.berkeley.edu>
2021-09-10 15:57:05 -0700

    update workflow to modify spreadsheet, update readme and commit repo

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # test-gh-actions
 Repository to test usage of GH actions
+
+This repository contains code that runs a GH Action as a cron job or on a push. It demonstrates accessing a Google sheet that has a Service Account with access to the sheet, using credentials stored in an encrypted json file that is decrypted based on a GH stored secret. 
+
+Here's a test of the functionality of the Action. 
+
+Number of sheet entries: 1
+Last entry processed: 10-08-2021 15:48:42
--- test.py
+++ test.py
@@ -15,6 +15,10 @@ import os.path
 from googleapiclient.discovery import build
 from google.oauth2.service_account import Credentials
 
+import numpy as np
+import os
+from datetime import datetime
+
 import pdb
 
 # If modifying these scopes, delete the file token.json.
@@ -22,15 +26,12 @@ SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 
 # The ID and range of a sample spreadsheet.
 SAMPLE_SPREADSHEET_ID = '1G264KrXYdp4Y0YQ4IpIetbQ2ItqEwOVrhZb-KkNMiPw'
-SAMPLE_RANGE_NAME = 'Sheet1!A2:B2'
+SAMPLE_RANGE_NAME = 'Sheet1!A1:D2000'
 SAMPLE_RANGE_NAME2 = 'Sheet1!A2:A3'
 
 def main():
-    """Shows basic usage of the Sheets API, including inserting values into 
-    a copy of the Fall 2020 registration spreadsheet.
+    """Shows basic usage of the Sheets API
     """
-    creds = None
-
     creds = Credentials.from_service_account_file('service_account.json', scopes = SCOPES)
     service = build('sheets', 'v4', credentials=creds)
 
@@ -43,10 +44,40 @@ def main():
     if not values:
         print('No data found.')
     else:
-        print('Field1, Field2:')
-        for row in values:
-            # Print columns A and E, which correspond to indices 0 and 4.
-            print('%s, %s' % (row[0], row[1]))
+        l = len(values)
+        print('Found %s rows' % l-1)
+        print('Last row is: %s, %s' % (values[l-1][0], values[l-1][1]))
 
+        if values[0][3] != "processed":
+            raise Exception("Fourth column in sheet is not 'processed'.")
+        lens = [len(x) for x in values]
+        unprocessed = np.where(np.array(lens) == 3)
+        if len(unprocessed[0]) >= 1):
+            ## update spreadsheet processed column
+            st = np.min(unprocessed) + 1
+            end = np.max(unprocessed) + 1
+            insertvalues = [['yes']] * (end-st+1)
+            body = {
+                'values': insertvalues
+            }
+
+            SAMPLE_RANGE_OUTPUT = 'Sheet1!D' + str(st) + ':D' + str(end)
+            result = sheet.values().update(
+                spreadsheetId=SAMPLE_SPREADSHEET_ID, range=SAMPLE_RANGE_OUTPUT,
+                valueInputOption='USER_ENTERED', body=body).execute()
+
+            ## modify README
+            now = datetime.now()
+            dt_string = now.strftime("%d-%m-%Y %H:%M:%S")
+            
+            numberLine = 'sed -i "s/^Number of sheet entries:.*/Number of sheet entries: ' + str(l-1) + '/" README.md'
+            dateLine = 'sed -i "s/^Last entry processed:.*/Last entry processed: ' + dt_string + '/" README.md'
+            
+            os.system(numberLine)
+            os.system(dateLine)
+
+        if(l == 2000):
+            raise Exception("Full spreadsheet not read")
+        
 if __name__ == '__main__':
     main()
